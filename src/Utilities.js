10f565998a38506c9645fc77a380c8e89aa6a4aa
mbastien <bastien.marc.dev@gmail.com>
2022-04-02 09:04:23 -0400

    refactored filterByYearHandler function. Added a conditional to ExpenseContainer component to handle an empty array of filtered expenses

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,12 +1,7 @@
 const filterByYearHandler = (expenses, year) => {
-    //console.log('Expenses:', expenses);
-    //console.log('Year to Filter By:', year);
 
-    const updatedData = expenses.filter(expense => {
-        return expense.date.getFullYear().toString() === year.toString();
-    });
+    return expenses.filter( expense => expense.date.getFullYear().toString() === year.toString());
 
-    return updatedData.length > 0? updatedData : expenses;
 }
 
 export { filterByYearHandler };
--- src/components/Expenses/ExpenseContainer.component.js
+++ src/components/Expenses/ExpenseContainer.component.js
@@ -10,7 +10,7 @@ function ExpensesContainer(props) {
     // Saving Initial Data into variable
     const expenses = props.items;
 
-    const [filterYearState, setFilterYearState] = useState('');
+    const [filterYearState, setFilterYearState] = useState('2019');
 
     // Function used to lift the filter state to be used in this component
     const saveFilterYearHandler = year => {    
@@ -19,21 +19,26 @@ function ExpensesContainer(props) {
 
     // Saving the filtered expenses data that will be used to dynamically generate expense items
     const filteredData = filterByYearHandler(expenses, filterYearState);
+    
+    let expensesContent = <p>No expenses found.</p>
+
+    if (filteredData.length > 0) {
+        expensesContent = filteredData.map( expense => (
+                <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}        
+                ></ExpenseItem>
+            )
+        )
+    }
 
     return (
         <div>
             <Card className='expenses'>
                 <ExpenseFilter selected={filterYearState} onSaveFilterYear={saveFilterYearHandler} />
-                {filteredData.map(expense => {
-                    return (
-                        <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}        
-                        ></ExpenseItem>
-                    )
-                })}
+                {expensesContent}
             </Card>
         </div>
     )
