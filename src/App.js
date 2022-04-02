c81008a7af483f28bd4388aa48cc67dc03dcc0d6
mbastien <bastien.marc.dev@gmail.com>
2022-04-02 08:14:50 -0400

    Moved the initial expense item state outside the app function and updated the variable name. Removed console.log

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, {useState} from 'react';
 import ExpenseContainer from "./components/Expenses/ExpenseContainer.component";
 import NewExpense from './components/NewExpense/NewExpense.component';
 
+
+const INITIAL_EXPENSE_STATE = [
+  {id: 'e1', title: 'Chase Credit Card', amount: 35.00, date: new Date(2020, 2, 30)},
+  {id: 'e2', title: 'Gas', amount: 13.00, date: new Date(2019, 5, 16)},
+  {id: 'e3', title: 'Pizza', amount: 12.68, date: new Date(2021, 1, 25)},
+  {id: 'e4', title: 'Hennessy', amount: 16.75, date: new Date(2021, 7, 30)},
+  {id: 'e5', title: 'Flatscreen 4K TV', amount: 754.75, date: new Date(2021, 6, 30)},
+];
+
 function App() {
 
-  const expenses = [
-    {id: 'e1', title: 'Chase Credit Card', amount: 35.00, date: new Date(2020, 2, 30)},
-    {id: 'e2', title: 'Gas', amount: 13.00, date: new Date(2019, 5, 16)},
-    {id: 'e3', title: 'Pizza', amount: 12.68, date: new Date(2021, 1, 25)},
-    {id: 'e4', title: 'Hennessy', amount: 16.75, date: new Date(2021, 7, 30)},
-    {id: 'e5', title: 'Flatscreen 4K TV', amount: 754.75, date: new Date(2021, 6, 30)},
-  ];
-
-  const [expenseState, setExpenseState] = useState(expenses);
-  console.log('Initial State:', expenseState);
+  const [expenseState, setExpenseState] = useState(INITIAL_EXPENSE_STATE);
 
   const addNewExpenseHandler = newExpense => {
 
@@ -28,8 +28,6 @@ function App() {
       return [newExpenseData, ...prevState]
     });
   }
-
-  console.log('After SetState:', expenseState)
 
   return (
     <div>
